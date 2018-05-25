43ed1235a7d002a2fc0ceba440e3635848b764e5
aviad cohen <aviad202000@yahoo.com>
2018-05-25 20:03:45 +0300

    move persons to an independant map have the tax rates be given as a list

--- xmlbased/src/main/java/pukteam/course/spring/taxes/main/Main.java
+++ xmlbased/src/main/java/pukteam/course/spring/taxes/main/Main.java
@@ -5,8 +5,9 @@ import org.apache.logging.log4j.Logger;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 import pukteam.course.spring.taxes.calculator.TaxCalculator;
-import pukteam.course.spring.taxes.calculator.TaxCalculatorImpl;
 import pukteam.course.spring.taxes.model.Person;
+
+import java.util.Map;
 
 public class Main {
 
@@ -17,18 +18,12 @@ public class Main {
         ApplicationContext ctx = new ClassPathXmlApplicationContext("app-context.xml");
 
         TaxCalculator calculator = ctx.getBean("calculator", TaxCalculator.class);
+        Map<String, Person> personList = ctx.getBean("personList", Map.class);
 
-        Person menash = ctx.getBean("menash", Person.class);
-        Person tikva = ctx.getBean("tikva", Person.class);
-        Person simcha = ctx.getBean("simcha", Person.class);
+        personList.forEach( (name, person) -> {
+            int taxRate = calculator.calc(person);
+            logger.info("{} has income of {} and needs to pay tax: {}", name, person.getIncome(), taxRate);
+        });
 
-        int taxRate = calculator.calc(menash);
-        logger.info("Menash has income of " + menash.getIncome() + " and needs to pay tax: " + taxRate);
-
-        taxRate = calculator.calc(tikva);
-        logger.info("Tikva has income of " + tikva.getIncome() + " and needs to pay tax: " + taxRate);
-
-        taxRate = calculator.calc(simcha);
-        logger.info("Simcha has income of " + simcha.getIncome() + " and needs to pay tax: " + taxRate);
     }
 }
